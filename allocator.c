9b2705991df2a8f6d030a799aefae3a392fde60f
Sam <slhdoss@gmail.com>
2014-08-17 21:02:25 +1000

    Update allocator.c

--- allocator.c
+++ allocator.c
@@ -17,9 +17,9 @@
 #define MAGIC_ALLOC    0xBEEFDEAD
 
 typedef unsigned char byte;
-typedef u_int32_t vlink_t;
-typedef u_int32_t vsize_t;
-typedef u_int32_t vaddr_t;
+typedef u_int32_t vlink_t;          // Memory index
+typedef u_int32_t vsize_t;          // Memory Size + Header 
+typedef u_int32_t vaddr_t;          // index of the first memeory bloc
 
 typedef struct free_list_header {
    u_int32_t magic;           // ought to contain MAGIC_FREE
@@ -30,14 +30,39 @@ typedef struct free_list_header {
 
 // Global data
 
-static byte *memory = NULL;   // pointer to start of suballocator memory
+static byte *memory = NULL;   // pointer to start of suballocator memory  ???after the header
 static vaddr_t free_list_ptr; // index in memory[] of first block in free list
 static vsize_t memory_size;   // number of bytes malloc'd in memory[]
 
 
-void sal_init(u_int32_t size)
-{
-   // TODO
+void sal_init(u_int32_t size) {
+    
+
+    //check that size is a power of 2, if not increment untill it is
+    if(sqrt((float)size) % 1 != 0){
+        while((sqrt((float)size) % 1 != 0)) {
+            size++;    
+        }
+        (u_int32_t) size 
+    }
+
+    // malloc new memory block
+    byte *memory = malloc(size);
+
+    // if there is insuficient memory for newMemBlock abort 
+    if(memory == NULL){
+        fprintf(stderr, "sal_init: insufficient memory");
+        abort();  
+    } else {
+        free_list_ptr = memory[1];  //traverse through the header to first bloc of memory
+        memory_size = sizeof(memory); //size of memory allocated
+                
+        Location -> magic = MAGIC_FREE; //?? don't know the purpose of this yet, general stats?? 
+        Location -> size = sizeof(newMemBlock);  // general stats ot be stored in the header??
+        Location -> next = free_list_ptr;   // stored index, initialized at the membloc post header start 
+        Location -> prev = free_list_ptr; // stoted index, initialized at the membloc post header start
+    }
+
 }
 
 void *sal_malloc(u_int32_t n)
