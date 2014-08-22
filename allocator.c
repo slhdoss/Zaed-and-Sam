3829620018f9e1d37ecc6d98be8a4e8094362637
shoomba <zaed.tayeb@gmail.com>
2014-08-22 16:04:22 +1000

    Update allocator.c

--- allocator.c
+++ allocator.c
@@ -17,7 +17,7 @@
 #define MAGIC_FREE     0xDEADBEEF
 #define MAGIC_ALLOC    0xBEEFDEAD
 
-#define NOT_FOUND -1;
+#define NOT_FOUND -1
 
 typedef unsigned char byte;
 typedef u_int32_t vlink_t;
@@ -42,6 +42,7 @@ static int num_free_blocks = 0; // how many free blocks we have
 static u_int32_t smallestPowerOfTwo (u_int32_t size);
 static u_int32_t getBestFreeRegionIndex (u_int32_t desired_size);
 static void splitFreeRegion (u_int32_t region_index, u_int32_t desired_size);
+static void merge (u_int32_t region_index);
 
 void sal_init(u_int32_t size) {
     if (memory == NULL) {
@@ -81,16 +82,17 @@ void *sal_malloc(u_int32_t n) {
     if (num_free_blocks == 0) {
         return NULL;
     }
+
     
     // desired size is just given plus the size of the header. 
-    u_int_32_t desired_size = n + HEADER_SIZE;
+    u_int32_t desired_size = n + HEADER_SIZE;
     
     // determine the 'best' region to allocate based on some policy
     // at the moment its just first-fit.
     u_int32_t chosen_region_index = getBestFreeRegionIndex (desired_size);
 
     // if there's no space, then return null
-    if (best_free_region_index == NOT_FOUND) { 
+    if (chosen_region_index == NOT_FOUND) { 
         return NULL; 
     }
 
@@ -98,33 +100,42 @@ void *sal_malloc(u_int32_t n) {
     splitFreeRegion(chosen_region_index, desired_size);
 
     // set the magic variable to 'allocated'
-    free_header_t * chosen_region_header = memory + chosen_region_index;
+    free_header_t * chosen_region_header = (free_header_t *) memory + chosen_region_index;
     chosen_region_header->magic = MAGIC_ALLOC;
 
     // remove the region from the free list by adjusting it's neighbours.
-    free_header_t * next_neighbour_header = memory + chosen_region_header->next;
-    free_header_t * prev_neighbour_header = memory + chosen_region_header->prev;
-    next_neighbour_header->prev = chosen_region_header->prev;
-    prev_neighbour_header->next = chosen_region_header->next;
+    if (num_free_blocks > 1) {
+        free_header_t * next_neighbour_header = (free_header_t *) memory + chosen_region_header->next;
+        free_header_t * prev_neighbour_header = (free_header_t *) memory + chosen_region_header->prev;
+        next_neighbour_header->prev = chosen_region_header->prev;
+        prev_neighbour_header->next = chosen_region_header->next;
+    }
 
-    setFreeListPtrToStart();
+    // we're assuming that free_list_ptr already pointed to the smallest index.
+    // so if the chosen region = free list ptr, then shifting it to next will maintain that
+    // if it isnt, then we dont need to change anything
+    // fuck yeah its an invariant.
+    if (num_free_blocks > 1 && chosen_region_index == free_list_ptr) {
+        free_list_ptr = chosen_region_header->next;
+    }
    
     num_free_blocks--;
     
     // finally, return the address of the allocated region (plus header size)
-    return memory + chosen_region_index + HEADER_SIZE; 
+    return (void *) memory + chosen_region_index + HEADER_SIZE; 
 }
 
 void sal_free(void *object) {
     
-    // find the header index of the object
-    object_Index = object - HEADER_SIZE;
-
     // establish a header point of reference
-    free_header_t * objectMemBlock = &(memory[object_Index]);
+    free_header_t * objectMemBlock = (free_header_t *) object - HEADER_SIZE;
+    
+    // find the header index of the object
+    u_int32_t object_Index = object - (void *) memory - HEADER_SIZE;
+    
 
     // Check requested header is valid
-    if( *(objectMemBlock) != MAGIC_ALLOC) {
+    if( objectMemBlock->magic != MAGIC_ALLOC) {
         fprintf(stderr, "Attempt to free non-allocated memory");
         abort();
     }
@@ -132,36 +143,42 @@ void sal_free(void *object) {
     // check if the entire block is allocated
     if(num_free_blocks == 0) { 
         objectMemBlock->next = object_Index;
-        objectMemBlock->previous = object_Index;
+        objectMemBlock->prev = object_Index;
         free_list_ptr = object_Index;
     // check if allocated bloc is the header with the smalled index
     } else if (object_Index < free_list_ptr) { 
-        free_header_t * flpMemBlock = &(memory[free_list_ptr])
+        free_header_t * flpMemBlock = (free_header_t *) &(memory[free_list_ptr]);
+        
         // Change index pointers
-        objectMemBlock->next = flpMemBlock - memory;
-        objectMemBlock->previous = flpMemBlock->previous;
-        flpMemBlock->previous = object_Index;            
+        objectMemBlock->next = free_list_ptr;
+        objectMemBlock->prev = flpMemBlock->prev;
+        flpMemBlock->prev = object_Index;
+                    
         free_list_ptr = object_Index;
     // All remaining cases
     } else {
-        free_header_t * neighbourFreeMemblock= &(memory[free_list_ptr]);
-        neighbourFreeMemblock_index = neighbourFreeMemblock- memory; 
+        free_header_t * neighbourFreeMemblock=(free_header_t *)  &(memory[free_list_ptr]);
+        u_int32_t neighbourFreeMemblock_index = free_list_ptr; 
+        
         // travers through free list untill an adjacent free memory block is found
         while(object_Index < neighbourFreeMemblock_index) {
             neighbourFreeMemblock_index = neighbourFreeMemblock-> next;
-            neighbourFreeMemblock = &(memory[neighbourFreeMemblock_index]);
+            neighbourFreeMemblock = (free_header_t *) &(memory[neighbourFreeMemblock_index]);
+            
             if (neighbourFreeMemblock_index == free_list_ptr){
                 break;    
             }
         }
         // Change index pointers
-        objectMemBlock -> next = neighbourFreeMemblock-> previous;      
-        neighbourFreeMemblock-> previous = object_Index;
+        objectMemBlock -> next = neighbourFreeMemblock-> prev;      
+        neighbourFreeMemblock-> prev = object_Index;
         objectMemBlock -> next = neighbourFreeMemblock_index;
     } 
 
     objectMemBlock->magic = MAGIC_FREE;
     num_free_blocks++;
+
+    merge (object_Index);
 }
 
 void sal_end(void) {
@@ -186,35 +203,101 @@ void sal_stats(void) {
 
 ////// Our things
 
-static void setFreeListPtrToStart () {
-    if (num_free_blocks < 1)
-    // traverse the list
-    // find smallest one
-    // set it to that
+static void merge (u_int32_t region_index) {
+    // If theres only 1 free block then nothing should happen
+    if (num_free_blocks < 2) {
+        return;
+    }
+
+    free_header_t * region_header = (free_header_t *) memory + region_index;
+    u_int32_t dest_region_index;
+    free_header_t * dest_region_header;
+
+    // If index is an even (inc. 0) multiple of size, then merge forward, otherwise merge backward.
+    if ((region_index / region_header->size) % 2 == 0) {
+        // we're going to try and merge it with the free region directly in front.
+        dest_region_index = region_header->next;
+        dest_region_header = (free_header_t *) memory + dest_region_index;
+
+        // If it's located directly in front, AND is the right size, then they can
+        // merge! (otherwise do nothing)
+        if ( (dest_region_index == region_index + region_header->size) 
+             && (dest_region_header->size == region_header->size) ) {
+
+            // so weve agreed that free_list_ptr always contains the smallest possible free index.
+            // this means that it never corresponds to the region directly in front (dest_index)
+            // so dont have to do anything about it. Fuck yeah
+
+            // adjust the size of the header;
+            region_header->size *= 2;
+
+            // adjust the free pointer list so everyone points to the right place
+            // ie, the region in front is removed
+            region_header->next = dest_region_header->next;
+            free_header_t * new_neighbour = (free_header_t *) memory + region_header->next;
+            new_neighbour->prev = region_index;
+
+            // number of free blocks has decreased
+            num_free_blocks--;
+
+            // there is a possibility that we can merge again! So let's just call
+            // merge again with region_index;
+            merge (region_index);
+        }
+
+    } else {
+        // we're going to try and merge it with the free region directly behind.
+        dest_region_index = region_header->prev;
+        dest_region_header =(free_header_t *)  memory + dest_region_index;
+
+        // If it's located directly in front of the previous region, 
+        // AND is the right size, then they can merge! (Otherwise do nothing)
+        if ( (region_index == dest_region_index + dest_region_header->size) 
+             && (dest_region_header->size == region_header->size) ) {
+
+            // so weve agreed that free_list_ptr always contains the smallest possible free index.
+            // this means that it never points to what we're on, since obviously theres a free 
+            // region behind us in memory.
+
+            // adjust the size of the previous header;
+            dest_region_header->size *= 2;
+
+            // adjust the free pointer list so everyone points to the right place
+            // ie, the current region is removed
+            dest_region_header->next = region_header->next;
+            free_header_t * new_neighbour =(free_header_t *)  memory + dest_region_header->next;
+            new_neighbour->prev = dest_region_index;
+
+            // number of free blocks has decreased
+            num_free_blocks--;
+
+            // there is a possibility that we can merge again! So let's just call
+            // merge again with dest_region_index;
+            merge (dest_region_index);
+        }
+    }
 }
 
-
 static void splitFreeRegion (u_int32_t region_index, u_int32_t desired_size) {
-    
-    free_header_t * region_header = memory + region_index;
+    free_header_t * region_header = (free_header_t *) memory + region_index;
 
     // while we can fit it within half the space of the chosen free region, 
     // divide the region in half.
     while (region_header->size / 2 >= desired_size) {
         // determine where the split will occur.
-        u_int32_t destination_index = memory + region_index + (region_header->size / 2);
+        u_int32_t destination_index = region_index + (region_header->size / 2);
 
         // create a new header to insert at destination_index.
         free_header_t new_header = { MAGIC_FREE, 
                                      region_header->size / 2, 
-                                     region_header->next
+                                     region_header->next,
                                      region_index };
       
         // insert the header
-        memcpy(destination_index, &new_header, HEADER_SIZE);
+        memcpy(memory + destination_index, &new_header, HEADER_SIZE);
        
         // adjust the prev index of the header 'in front of' (this_free_region->next) the free region we're splitting.
-        free_header_t * neighbour = memory + region_header->next;
+        free_header_t * neighbour = (free_header_t *) memory + region_header->next;
         neighbour->prev = destination_index;
        
         // adjust the header of the region we're splitting to account for the changes.
@@ -229,21 +312,21 @@ static void splitFreeRegion (u_int32_t region_index, u_int32_t desired_size) {
 // but that could change.
 static u_int32_t getBestFreeRegionIndex (u_int32_t desired_size) {
     u_int32_t curr_free_region_index        = free_list_ptr;
-    free_header_t * curr_free_region_header = memory + curr_free_region_index;
+    free_header_t * curr_free_region_header = (free_header_t *) memory + curr_free_region_index;
    
     // loop through all free regions until we find one that fits.
     while (curr_free_region_header->size < desired_size) {
         // get the index of the next free region.
-        curr_free_region_index = memory + curr_free_region_header->next;
+        curr_free_region_index = curr_free_region_header->next;
        
         // if we're back to free_list_ptr, there's no space availeble
         // so just return NULL.
-        if (curr_free_region_index == free_list_ptr)) {
+        if (curr_free_region_index == free_list_ptr) {
             return NOT_FOUND;
         } 
        
         // otherwise set the header pointer to the header of the next region.
-        curr_free_region_header = memory + curr_free_region_index;
+        curr_free_region_header = (free_header_t *) memory + curr_free_region_index;
    }
 
    return curr_free_region_index;       
